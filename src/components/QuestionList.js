f808a92c00b7ccbf4ededce2f61c0ed5ff363e41
Ian Kabaka <iankabaka1@gmail.com>
2024-11-11 12:29:53 +0300

    creating the callback function and adding it once the Delete on the server is successful

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -5,7 +5,15 @@ function QuestionList({ quiz, setQuiz }) {
     fetch("http://localhost:4000/questions")
       .then((res) => res.json())
       .then((data) => setQuiz(data));
-  }, []);
+  }, [setQuiz]);
+
+  const handeDelete = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(setQuiz(quiz.filter((question) => question.id !== id)));
+  };
   const newQuizzes = quiz.map((oneQuiz) => {
     return <li key={oneQuiz.id}>{oneQuiz.prompt}</li>;
   });
